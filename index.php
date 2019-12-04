2c03dd7236700fd927f6f8fe5207795858c85f85
Yara <ybn242@gmail.com>
2019-12-04 16:41:07 -0300

    separamos os arquivos

--- index.php
+++ index.php
@@ -1,62 +1,219 @@
 <!DOCTYPE html>
-<html>
-<?php include 'incial.php';
-?>
-  <!-- about -->
-  <section id="section-about" class="section appear clearfix">
-    <div class="container">
 
-      <div class="row mar-bot40">
-        <div class="col-md-offset-3 col-md-6">
-          <div class="section-header">
-            <h2 class="section-heading animated" data-animation="bounceInUp">Behind Vlava</h2>
-            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet consectetur, adipisci velit, sed quia non numquam.</p>
-          </div>
+
+<head>
+  <!-- BASICS -->
+  <meta charset='utf-8'>
+  <title>Vlava - One page responsive bootstrap site template</title>
+  <meta name='description' content=''>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <link rel='stylesheet' type='text/css' href='js/rs-plugin/css/settings.css' media='screen'>
+  <link rel='stylesheet' type='text/css' href='css/isotope.css' media='screen'>
+  <link rel='stylesheet' href='css/flexslider.css' type='text/css'>
+  <link rel='stylesheet' href='js/fancybox/jquery.fancybox.css' type='text/css' media='screen'>
+  <link rel='stylesheet' href='css/bootstrap.css'>
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Noto+Serif:400,400italic,700|Open+Sans:300,400,600,700'>
+
+  <link rel='stylesheet' href='css/style.css'>
+  <!-- skin -->
+  <link rel='stylesheet' href='skin/default.css'>
+  <!-- =======================================================
+    Theme Name: Vlava
+    Theme URL: https://bootstrapmade.com/vlava-free-bootstrap-one-page-template/
+    Author: BootstrapMade.com
+    Author URL: https://bootstrapmade.com
+  ======================================================= -->
+
+</head>
+
+<body>
+  <section id='header' class='appear'></section>
+  <div class='navbar navbar-fixed-top' role='navigation' data-0='line-height:100px; height:100px; background-color:rgba(0,0,0,0.3);' data-300='line-height:60px; height:60px; background-color:rgba(5, 42, 62, 1);'>
+    <div class='container'>
+      <div class='navbar-header'>
+        <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
+          <span class='fa fa-bars color-white'></span>
+        </button>
+        <div class='navbar-logo'>
+          <a href='index.html'><img data-0='width:155px;' data-300=' width:120px;' src='img/logo.png' alt=''></a>
         </div>
       </div>
-
-      <div class="row align-center mar-bot40">
-        <div class="col-md-3">
-          <div class="team-member">
-            <figure class="member-photo"><img src="img/team/member1.jpg" alt=""></figure>
-            <div class="team-detail">
-              <h4>Jason Doe</h4>
-              <span>User experiences</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="team-member">
-            <figure class="member-photo"><img src="img/team/member2.jpg" alt=""></figure>
-            <div class="team-detail">
-              <h4>Timothy Clark</h4>
-              <span>Web developer</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="team-member">
-            <figure class="member-photo"><img src="img/team/member3.jpg" alt=""></figure>
-            <div class="team-detail">
-              <h4>Vicky Tan</h4>
-              <span>Web designer</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="team-member">
-            <figure class="member-photo"><img src="img/team/member4.jpg" alt=""></figure>
-            <div class="team-detail">
-              <h4>Kevin Peterson</h4>
-              <span>UI designer</span>
-            </div>
-          </div>
-        </div>
+      <div class='navbar-collapse collapse'>
+        <ul class='nav navbar-nav' data-0='margin-top:20px;' data-300='margin-top:5px;'>
+          <li class='active'><a href='index.html'>Home</a></li>
+          <li><a href='#section-about'>About</a></li>
+          <li><a href='#section-works'>Portfolio</a></li>
+          <li><a href='#section-contact'>Contact</a></li>
+        </ul>
       </div>
+      <!--/.navbar-collapse -->
+    </div>
+  </div>
 
+  <section id='intro'>
+    <div class='intro-content'>
+      <h2>Welcome to Vlava!</h2>
+      <h3>Lorem ipsum dolor sit amet, elit persecuti efficiendi</h3>
+      <div>
+        <a href='#section-services' class='btn-get-started scrollto'>Get Started</a>
+      </div>
     </div>
   </section>
-  <!-- /about -->
+
+  <!-- services -->
+  <section id='section-services' class='section pad-bot30 bg-white'>
+    <div class='container'>
+
+      <div class='row mar-bot40'>
+        <div class='col-lg-4'>
+          <div class='hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a mar-top20'>
+            <div class='float-left mar-right20'>
+              <a href='#' class='hi-icon hi-icon-screen'>screen</a>
+            </div>
+          </div>
+          <h3 class='text-bold'>Responsive</h3>
+          <p>Lorem ipsum dolor sit amet, elit persecuti efficiendi sit ad.</p>
+
+          <div class='clear'></div>
+        </div>
+
+        <div class='col-lg-4'>
+          <div class='hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a mar-top20'>
+            <div class='float-left mar-right20'>
+              <a href='#' class='hi-icon hi-icon-location'>location</a>
+            </div>
+          </div>
+          <h3 class='text-bold'>Multi usage</h3>
+          <p>Lorem ipsum dolor sit amet, elit persecuti efficiendi sit ad.</p>
+
+          <div class='clear'></div>
+        </div>
+
+        <div class='col-lg-4'>
+          <div class='hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a mar-top20'>
+            <div class='float-left mar-right20'>
+              <a href='#' class='hi-icon hi-icon-images'>images</a>
+            </div>
+          </div>
+          <h3 class='text-bold'>Beautiful</h3>
+          <p>Lorem ipsum dolor sit amet, elit persecuti efficiendi sit ad.</p>
+
+          <div class='clear'></div>
+        </div>
+
+      </div>
+      <div class='row'>
+        <div class='col-lg-4'>
+          <div class='hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a mar-top20'>
+            <div class='float-left mar-right20'>
+              <a href='#' class='hi-icon hi-icon-archive'>archive</a>
+            </div>
+          </div>
+          <h3 class='text-bold'>Valid HTML5</h3>
+          <p>Lorem ipsum dolor sit amet, elit persecuti efficiendi sit ad.</p>
+
+          <div class='clear'></div>
+        </div>
+
+        <div class='col-lg-4'>
+          <div class='hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a mar-top20'>
+            <div class='float-left mar-right20'>
+              <a href='#' class='hi-icon hi-icon-contract'>Mobile</a>
+            </div>
+          </div>
+          <h3 class='text-bold'>Modern</h3>
+          <p>Lorem ipsum dolor sit amet, elit persecuti efficiendi sit ad.</p>
+
+          <div class='clear'></div>
+        </div>
+
+        <div class='col-lg-4'>
+          <div class='hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a mar-top20'>
+            <div class='float-left mar-right20'>
+              <a href='#' class='hi-icon hi-icon-clock'>Faster</a>
+            </div>
+          </div>
+          <h3 class='text-bold'>Fast loading</h3>
+          <p>Lorem ipsum dolor sit amet, elit persecuti efficiendi sit ad.</p>
+
+          <div class='clear'></div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <!-- spacer section:testimonial -->
+  <section id='testimonials' class='section' data-stellar-background-ratio='0.5'>
+    <div class='container'>
+      <div class='row'>
+        <div class='col-lg-12'>
+          <div class='align-center'>
+            <div class='flexslider testimonials-slider'>
+              <ul class='slides'>
+                <li>
+                  <div class='testimonial clearfix'>
+                    <div class='mar-bot20'>
+                      <img alt='' src='img/testimonial/testimonial1.png' class='img-circle'>
+                    </div>
+                    <i class='fa fa-quote-left fa-5x'></i>
+                    <h5>
+                        Nunc velit risus, dapibus non interdum quis, suscipit nec dolor. Vivamus tempor tempus mauris vitae fermentum. In vitae nulla lacus. Sed sagittis tortor vel arcu sollicitudin nec tincidunt metus suscipit.Nunc velit risus, dapibus non interdum.
+                      </h5>
+                    <br/>
+                    <span class='author'>&mdash; SARAH DOE <a href='#'>www.siteurl.com</a></span>
+                  </div>
+                </li>
+
+                <li>
+                  <div class='testimonial clearfix'>
+                    <div class='mar-bot20'>
+                      <img alt='' src='img/testimonial/testimonial2.png' class='img-circle'>
+                    </div>
+                    <i class='fa fa-quote-left fa-5x'></i>
+                    <h5>
+                        Nunc velit risus, dapibus non interdum quis, suscipit nec dolor. Vivamus tempor tempus mauris vitae fermentum. In vitae nulla lacus. Sed sagittis tortor vel arcu sollicitudin nec tincidunt metus suscipit.Nunc velit risus, dapibus non interdum.
+                        </h5>
+                    <br/>
+                    <span class='author'>&mdash; NICOLE DOE <a href='#'>www.siteurl.com</a></span>
+                  </div>
+                </li>
+                <li>
+                  <div class='testimonial clearfix'>
+                    <div class='mar-bot20'>
+                      <img alt='' src='img/testimonial/testimonial3.png' class='img-circle'>
+                    </div>
+                    <i class='fa fa-quote-left fa-5x'></i>
+                    <h5>
+                      Nunc velit risus, dapibus non interdum quis, suscipit nec dolor. Vivamus tempor tempus mauris vitae fermentum. In vitae nulla lacus. Sed sagittis tortor vel arcu sollicitudin nec tincidunt metus suscipit.Nunc velit risus, dapibus non interdum.
+                      </h5>
+                    <br/>
+                    <span class='author'>&mdash; DASON KRUN <a href='#'>www.siteurl.com</a></span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  
+
+
+
+
+
+<?php  
+//include 'incial.php';
+
+?>
+<?php include 'sobre.php';
+
+?>
+
+
 
   <!-- spacer section:stats -->
   <section id="parallax1" class="section" data-stellar-background-ratio="0.5">
---
+++ sobre.php
@@ -0,0 +1,58 @@
+<?php
+echo "
+  <!-- about -->
+  <section id='section-about' class='section appear clearfix'>
+    <div class='container'>
+
+      <div class='row mar-bot40'>
+        <div class='col-md-offset-3 col-md-6'>
+          <div class='section-header'>
+            <h2 class='section-heading animated' data-animation='bounceInUp'>Behind Vlava</h2>
+            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet consectetur, adipisci velit, sed quia non numquam.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class='row align-center mar-bot40'>
+        <div class='col-md-3'>
+          <div class='team-member'>
+            <figure class='member-photo'><img src='img/team/member1.jpg' alt=''></figure>
+            <div class='team-detail'>
+              <h4>Jason Doe</h4>
+              <span>User experiences</span>
+            </div>
+          </div>
+        </div>
+        <div class='col-md-3'>
+          <div class='team-member'>
+            <figure class='member-photo'><img src='img/team/member2.jpg' alt=''></figure>
+            <div class='team-detail'>
+              <h4>Timothy Clark</h4>
+              <span>Web developer</span>
+            </div>
+          </div>
+        </div>
+        <div class='col-md-3'>
+          <div class='team-member'>
+            <figure class='member-photo'><img src='img/team/member3.jpg' alt=''></figure>
+            <div class='team-detail'>
+              <h4>Vicky Tan</h4>
+              <span>Web designer</span>
+            </div>
+          </div>
+        </div>
+        <div class='col-md-3'>
+          <div class='team-member'>
+            <figure class='member-photo'><img src='img/team/member4.jpg' alt=''></figure>
+            <div class='team-detail'>
+              <h4>Kevin Peterson</h4>
+              <span>UI designer</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+  <!-- /about -->
+  ";
